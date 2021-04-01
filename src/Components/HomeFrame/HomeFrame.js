4f06d893d36b7c886075320af5188af2de9218be
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-04-01 23:31:31 +0530

    Nav

---
+++ src/Components/HomeFrame/HomeFrame.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './HomeFrame.scss'
+export const HomeFrame = () => {
+    return (
+        <div className="all">
+            <div className="navbar">
+                <div className="item">About</div>
+                <div className="item">Experiences</div>
+                <div className="item">Education</div>
+                <div className="item">Skills</div>
+                <div className="item">Project</div>
+                <div className="item">Contact</div>
+            </div>
+            <div>
+                <hr/>
+            </div>
+        </div>
+    )
+}
